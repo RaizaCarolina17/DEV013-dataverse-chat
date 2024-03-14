db4a76689be6e6f43bc990d5ca06177a92c6d02a
Raiza Gatica <raizagatica@gmail.com>
2024-03-14 01:26:54 -0300

    conexión del componente boton que esta en en chat individual al componente de modal para api

--- src/Components/ButtonChat.js
+++ src/Components/ButtonChat.js
@@ -1,13 +1,17 @@
-//import { navigateTo } from "../router.js";
+import { ApiModal } from "./ApiModal.js";
 
 export const createBtnChat = () => {
   const btnChatInd = document.createElement("button");
   btnChatInd.id = "button-chatIndividual";
   btnChatInd.className = "button-chat";
   btnChatInd.textContent = "CHAT"; 
+
+  //Agregar evento
   btnChatInd.addEventListener("click", () => {
-
-
+    const apiModal = ApiModal(); // Crear una instancia del modal
+    document.body.appendChild(apiModal); // Agregar el modal al cuerpo del documento
+    apiModal.showModal(); // Mostrar el modal
   });
+
   return btnChatInd;
 };
